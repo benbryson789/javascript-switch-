5016a41d63c8fe700a8ec10bb2a4490932a02ab7
Debra Shelby <deborashelby@gmail.com>
2022-03-03 22:17:59 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let today = 'friday';
+switch(today){
+    case 'monday':
+        console.log("Here we go again....")
+        break;
+    case 'tuesday':
+        console.log("Are we done yet?")
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log("Getting a bit excited...")
+        break;
+    default:
+        console.log("I love weekends!")
+}
